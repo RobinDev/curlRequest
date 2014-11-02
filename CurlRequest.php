14e2c4242caae0a1d4f8a683d8997aad438f08e6
RobinDev <contact@robin-d.fr>
2014-11-02 12:50:51 +0100

    Few modifications to be PSR compliant

--- CurlRequest.php
+++ CurlRequest.php
@@ -1,45 +1,58 @@
 <?php
-
+namespace rOpenDev\curl;
 
 /**
- * PHP POO cURL wrapper (PSR compatible for autoloading).
+ * PHP POO cURL wrapper
  * Make it easy to request a URL (or few)
- *
+ * PSR-2 Coding Style, PSR-4 Autoloading
  *
  * PHP version 5
  *
- * @author     Original Author Robin <contact@robin-d.fr>
+ * @author     Robin <contact@robin-d.fr>
  * @link       https://github.com/RobinDev/curlRequest
  * @since      File available since Release 2014.04.29
  */
 
-namespace rOpenDev\curl;
+class CurlRequest
+{
 
-class CurlRequest {
+    /**
+     * @var bool $headerOnly If set to true (via self::setReturnHeaderOnly()), headers only are returned (via self::execute())
+     */
+    protected $headerOnly = false;
 
-	/**
-	 * If set to true (via self::setReturnHeaderOnly()), headers only are returned (via self::execute())
-	 * @var bool
-	 */
-	protected $headerOnly = false;
-
-	/**
-	 * @var Ressource containing cURL Session
-	 */
+    /**
+     * @var Ressource $ch containing cURL Session
+     */
     public static $ch;
 
-    protected $gzip=false,$rHeader=false;
+    /*
+     * @var bool $gzip If set to true (via self::setEncodingGzip($gzip)), self::execute() will try to uncompress cURL output
+     */
+    protected $gzip=false;
 
-    function __construct($url, $usePreviousSession=false)
+    /**
+     * @var bool $rHeader If set to true (via self::setReturnHeader()), self::execute() will extract HTTP header
+     *                    from the cURL output and stock it in self::$header wich can be get with self::getHeader()
+     */
+    protected $rHeader=false;
+
+    /**
+     * Constructor
+     *
+     * @param $url string The URL to request
+     * @param $usePreviousSession bool If the query must use the previous session (so using same connexion if it's the same host)
+     */
+    public function __construct($url, $usePreviousSession=false)
     {
-		if($usePreviousSession === false || !isset(self::$ch)) {
-			self::$ch = curl_init($url);
-		}
-		else {
-			curl_reset(self::$ch);
-			$this->setOpt(CURLOPT_URL, $url);
-		}
-        $this->setOpt(CURLOPT_RETURNTRANSFER, true);
+        if($usePreviousSession === false || !isset(self::$ch)) {
+            self::$ch = curl_init($url);
+        }
+        else {
+            curl_reset(self::$ch);
+            $this->setOpt(CURLOPT_URL, $url);
+        }
+        $this->setOpt(CURLOPT_RETURNTRANSFER, 1);
     }
 
     /**
@@ -50,234 +63,234 @@ class CurlRequest {
      *
      * @return self
      */
-    function setUrl($url, $reset = false)
+    public function setUrl($url, $reset = false)
     {
-		if($resest) {
-			curl_reset(self::$ch);
-		}
-		$this->setOpt(CURLOPT_URL, $url);
-		return $this;
-	}
+        if($resest) {
+            curl_reset(self::$ch);
+        }
+        $this->setOpt(CURLOPT_URL, $url);
+        return $this;
+    }
 
-	/**
-	 * Add a cURL's option
-	 *
-	 * @param $option const cURL Predefined Constant
-	 * @param $value mixed
-	 *
-	 * @return self
-	 */
-    function setOpt($option, $value)
+    /**
+     * Add a cURL's option
+     *
+     * @param $option const cURL Predefined Constant
+     * @param $value mixed
+     *
+     * @return self
+     */
+    public function setOpt($option, $value)
     {
         curl_setopt(self::$ch, $option, $value);
         return $this;
     }
 
-	/**
-	 * A short way to set some classic options to cURL a web page
-	 *
-	 * @return self
-	 */
-    function setDefaultGetOptions($connectTimeOut = 5, $timeOut = 10, $dnsCacheTimeOut = 600, $followLocation = true, $maxRedirs = 5)
+    /**
+     * A short way to set some classic options to cURL a web page
+     *
+     * @return self
+     */
+    public function setDefaultGetOptions($connectTimeOut = 5, $timeOut = 10, $dnsCacheTimeOut = 600, $followLocation = true, $maxRedirs = 5)
     {
-        $this->setOpt(CURLOPT_AUTOREFERER, true)
+        $this->setOpt(CURLOPT_AUTOREFERER,       1)
              ->setOpt(CURLOPT_FOLLOWLOCATION,    $followLocation)
              ->setOpt(CURLOPT_MAXREDIRS,         $maxRedirs)
              ->setOpt(CURLOPT_CONNECTTIMEOUT,    $connectTimeOut)
              ->setOpt(CURLOPT_DNS_CACHE_TIMEOUT, $dnsCacheTimeOut)
              ->setOpt(CURLOPT_TIMEOUT,           $timeOut)
-             ->setOpt(CURLOPT_SSL_VERIFYPEER, false);
+             ->setOpt(CURLOPT_SSL_VERIFYPEER,    0);
         return $this;
     }
 
-	/**
-	 * A short way to set some classic options to cURL a web page quickly (but lossing some data like header, cookie...)
-	 *
-	 * @return self
-	 */
-    function setDefaultSpeedOptions()
+    /**
+     * A short way to set some classic options to cURL a web page quickly (but lossing some data like header, cookie...)
+     *
+     * @return self
+     */
+    public function setDefaultSpeedOptions()
     {
-        $this->setOpt(CURLOPT_SSL_VERIFYHOST, false)
-             ->setOpt(CURLOPT_SSL_VERIFYPEER, false)
-             ->setOpt(CURLOPT_HEADER, false)
-             ->setOpt(CURLOPT_COOKIE, false)
-             ->setOpt(CURLOPT_MAXREDIRS, 1);
+        $this->setOpt(CURLOPT_SSL_VERIFYHOST, 0)
+             ->setOpt(CURLOPT_SSL_VERIFYPEER, 0)
+             ->setOpt(CURLOPT_HEADER,         0)
+             ->setOpt(CURLOPT_COOKIE,         0)
+             ->setOpt(CURLOPT_MAXREDIRS,      1);
         return $this;
     }
 
-	/**
-	 * Call it if you want header informations.
-	 * After self::execute(), you would have this informations with getHeader();
-	 *
-	 * @return self
-	 */
-    function setReturnHeader()
+    /**
+     * Call it if you want header informations.
+     * After self::execute(), you would have this informations with getHeader();
+     *
+     * @return self
+     */
+    public function setReturnHeader()
     {
-        $this->setOpt(CURLOPT_HEADER, true);
+        $this->setOpt(CURLOPT_HEADER, 1);
         $this->rHeader = true;
         return $this;
     }
 
-	/**
-	 * Call it if you want header informations only.
-	 * After self::execute(), you would have this informations with getHeader();
-	 *
-	 * @return self
-	 */
-    function setReturnHeaderOnly()
+    /**
+     * Call it if you want header informations only.
+     * After self::execute(), you would have this informations with getHeader();
+     *
+     * @return self
+     */
+    public function setReturnHeaderOnly()
     {
-		$this->headerOnly = true;
-		$this->setOpt(CURLOPT_HEADER,	true)
-		     ->setOpt(CURLOPT_NOBODY,	true);
-		return $this;
-	}
+        $this->headerOnly = true;
+        $this->setOpt(CURLOPT_HEADER,    1)
+             ->setOpt(CURLOPT_NOBODY,    1);
+        return $this;
+    }
 
     /**
      * An self::setOpt()'s alias to add a cookie to your request
      *
      * @param string $cookie
      *
-	 * @return self
+     * @return self
      */
-    function setCookie($cookie)
+    public function setCookie($cookie)
     {
         $this->setOpt(CURLOPT_COOKIE, $cookie);
         return $this;
     }
 
-	/**
+    /**
      * An self::setOpt()'s alias to add a referrer to your request
      *
      * @param string $referrer
      *
-	 * @return self
+     * @return self
      */
-    function setReferrer($referrer)
+    public function setReferrer($referrer)
     {
         $this->setOpt(CURLOPT_REFERER, $referrer);
         return $this;
     }
 
-	/**
+    /**
      * An self::setOpt()'s alias to add an user-agent to your request
      *
      * @param string $ua
      *
-	 * @return self
+     * @return self
      */
-    function setUserAgent($ua)
+    public function setUserAgent($ua)
     {
         $this->setOpt(CURLOPT_USERAGENT, $ua);
         return $this;
     }
 
-	/**
+    /**
      * An self::setUserAgent()'s alias to add an user-agent wich correspond to a Destkop PC
      *
-	 * @return self
+     * @return self
      */
-    function setDestkopUserAgent()
+    public function setDestkopUserAgent()
     {
         $this->setUserAgent('Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:28.0) Gecko/20100101 Firefox/28.0');
         return $this;
     }
 
-	/**
+    /**
      * An self::setUserAgent()'s alias to add an user-agent wich correspond to a mobile
      *
-	 * @return self
+     * @return self
      */
-    function setMobileUserAgent()
+    public function setMobileUserAgent()
     {
         $this->setUserAgent('Mozilla/5.0 (Linux; U; Android 2.2.1; en-ca; LG-P505R Build/FRG83) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1');
         return $this;
     }
 
-	/**
+    /**
      * An self::setUserAgent()'s alias to add an user-agent wich correspond to a webrowser without javascript
      *
-	 * @return self
+     * @return self
      */
-    function setLessJsUserAgent()
+    public function setLessJsUserAgent()
     {
         $this->setUserAgent('NokiaN70-1/5.0609.2.0.1 Series60/2.8 Profile/MIDP-2.0 Configuration/CLDC-1.1 UP.Link/6.3.1.13.0');
         return $this;
     }
 
-	/**
-	 * A short way to set post's options to cURL a web page
-	 *
-	 * @param $post_array array containing data (key=>vvalue) to post
-	 *
-	 * @return self
-	 */
-    function setPost($post_array)
+    /**
+     * A short way to set post's options to cURL a web page
+     *
+     * @param $post_array array containing data (key=>vvalue) to post
+     *
+     * @return self
+     */
+    public function setPost($post_array)
     {
         $this->setOpt(CURLOPT_CUSTOMREQUEST, 'POST');
-        $this->setOpt(CURLOPT_POST, true);
+        $this->setOpt(CURLOPT_POST, 1);
         $this->setOpt(CURLOPT_POSTFIELDS, http_build_query($post_array));
         return $this;
     }
 
-	/**
-	 * If you want to request the URL and hope get the result gzipped.
-	 * The output will be automatically uncompress with execute();
-	 *
-	 * @return self
-	 */
-    function setEncodingGzip()
+    /**
+     * If you want to request the URL and hope get the result gzipped.
+     * The output will be automatically uncompress with execute();
+     *
+     * @return self
+     */
+    public function setEncodingGzip($decode = false)
     {
         $this->setOpt(CURLOPT_ENCODING, 'gzip, deflate');
-        $this->gzip = true;
+        $this->gzip = $decode;
         return $this;
     }
 
-	/**
-	 * If you want to request the URL with a http proxy (public or private)
-	 *
-	 * @param $proxy string IP:PORT[:LOGIN:PASSWORD]
-	 *
-	 * @return self
-	 */
-    function setProxy($proxy)
+    /**
+     * If you want to request the URL with a http proxy (public or private)
+     *
+     * @param $proxy string IP:PORT[:LOGIN:PASSWORD]
+     *
+     * @return self
+     */
+    public function setProxy($proxy)
     {
-		if(!empty($proxy)) {
-			$proxy = explode(':', $proxy);
-			$this->setOpt(CURLOPT_HTTPPROXYTUNNEL, true);
-			$this->setOpt(CURLOPT_PROXY, $proxy[0].':'.$proxy[1]);
-			if(isset($proxy[2])) {
-				$this->setOpt(CURLOPT_PROXYUSERPWD, $proxy[2].':'.$proxy[3]);
-			}
-		}
-		return $this;
+        if(!empty($proxy)) {
+            $proxy = explode(':', $proxy);
+            $this->setOpt(CURLOPT_HTTPPROXYTUNNEL, 1);
+            $this->setOpt(CURLOPT_PROXY, $proxy[0].':'.$proxy[1]);
+            if(isset($proxy[2])) {
+                $this->setOpt(CURLOPT_PROXYUSERPWD, $proxy[2].':'.$proxy[3]);
+            }
+        }
+        return $this;
     }
 
-	/**
-	 * Execute the request
-	 *
-	 * @return string wich is the request's result without the header (you can obtain with self::getHeader() now)
-	 */
-    function execute()
+    /**
+     * Execute the request
+     *
+     * @return string wich is the request's result without the header (you can obtain with self::getHeader() now)
+     */
+    public function execute()
     {
-		if($this->headerOnly) {
-			return $this->header = curl_exec(self::$ch);
-		}
-		$html = curl_exec(self::$ch);
-		//if($this->gzip) $html = gzdecode ($html);
+        if($this->headerOnly) {
+            return $this->header = curl_exec(self::$ch);
+        }
+        $html = curl_exec(self::$ch);
+        //if($this->gzip) $html = gzdecode ($html);
 
-		if($this->rHeader) {
-			$this->header = substr($html, 0, $sHeader=curl_getinfo(self::$ch, CURLINFO_HEADER_SIZE));
-			$html = substr($html, $sHeader);
-		}
+        if($this->rHeader) {
+            $this->header = substr($html, 0, $sHeader=curl_getinfo(self::$ch, CURLINFO_HEADER_SIZE));
+            $html = substr($html, $sHeader);
+        }
         return $html;
     }
 
-	/**
-	 * Return header's data return by the request
-	 *
-	 * @return array containing header's data
-	 */
-    function getHeader()
+    /**
+     * Return header's data return by the request
+     *
+     * @return array containing header's data
+     */
+    public function getHeader()
     {
         if(isset($this->header))
             return self::http_parse_headers($this->header);
@@ -290,7 +303,7 @@ class CurlRequest {
      *
      * @return null|array containing the cookies
      */
-    function getCookies()
+    public function getCookies()
     {
         if(isset($this->header)) {
             $header = $this->getHeader();
@@ -300,32 +313,32 @@ class CurlRequest {
         }
     }
 
-	/**
-	 * Return the last error number (curl_errno)
-	 *
-	 * @return int the error number or 0 (zero) if no error occurred.
-	 */
-    function hasError()
+    /**
+     * Return the last error number (curl_errno)
+     *
+     * @return int the error number or 0 (zero) if no error occurred.
+     */
+    public function hasError()
     {
         return curl_errno(self::$ch);
     }
 
-	/**
-	 * Return a string containing the last error for the current session (curl_error)
-	 *
-	 * @return string the error message or '' (the empty string) if no error occurred.
-	 */
-    function getErrors()
+    /**
+     * Return a string containing the last error for the current session (curl_error)
+     *
+     * @return string the error message or '' (the empty string) if no error occurred.
+     */
+    public function getErrors()
     {
         return curl_error(self::$ch);
     }
 
-	/**
-	 * Get information regarding the request
-	 *
-	 * @return bool|array an associative array with the following elements (which correspond to opt), or FALSE on failure
-	 */
-    function getInfo()
+    /**
+     * Get information regarding the request
+     *
+     * @return bool|array an associative array with the following elements (which correspond to opt), or FALSE on failure
+     */
+    public function getInfo()
     {
         return curl_getinfo(self::$ch);
     }
@@ -343,11 +356,11 @@ class CurlRequest {
      *
      * @return bool|array an array on success or FALSE on failure.
      */
-    static function http_parse_headers($raw_headers)
+    public static function http_parse_headers($raw_headers)
     {
-		if (function_exists('http_parse_headers')) {
-			http_parse_headers($raw_headers);
-		}
+        if (function_exists('http_parse_headers')) {
+            http_parse_headers($raw_headers);
+        }
 
         $headers = [];
         $key = '';
